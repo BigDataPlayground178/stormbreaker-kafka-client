2a01acd451c3a24c7d5fc276b446945aa7b15e76
Alessio Moretti <alessio.moretti@live.it>
2018-06-27 22:58:02 +0200

    First commit for Stormbreaker Kafka producer client

---
+++ client.py
@@ -0,0 +1,7 @@
+"""
+This is the main of the client ingesting events into Kafka to power 
+the Stormbreak Flink datastream processing platform 
+"""
+
+from confluent_kafka import Producer
+
---
+++ constants.py
@@ -0,0 +1,11 @@
+"""
+Constants of the Kafka producer client
+"""
+
+# topic 
+TOPIC_FRIENDSHIP = "friendships"
+TOPIC_POSTS      = "posts"
+TOPIC_COMMENTS   = "comments"
+
+# Kafka server configuration (assuming default port configuration)
+KAFKA_BOOTSTRAP_SERVER = "localhost"
