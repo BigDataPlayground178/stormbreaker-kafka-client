0fbe27457bd62ed14c2201c5dd64a7edb0a6e30a
Alessio Moretti <alessio.moretti@live.it>
2018-06-30 19:54:16 +0200

    Fixing spotify/kafka docker utils

--- dockerutils/spotify_kafka.sh
+++ dockerutils/spotify_kafka.sh
@@ -1,5 +1,5 @@
-docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=`docker-machine ip \`docker-machine active\`` --env ADVERTISED_PORT=9092 spotify/kafka
-export KAFKA=`docker-machine ip \`docker-machine active\``:9092
-kafka-console-producer.sh --broker-list $KAFKA --topic test
-export ZOOKEEPER=`docker-machine ip \`docker-machine active\``:2181
-kafka-console-consumer.sh --zookeeper $ZOOKEEPER --topic test
+docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 spotify/kafka
+export KAFKA=localhost:9092
+kafka-console-producer.sh --broker-list $KAFKA --topic topic
+export ZOOKEEPER=localhost:2181
+kafka-console-consumer.sh --zookeeper $ZOOKEEPER --topic topic
