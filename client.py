52f106115dda39eb07357027942055f9c508b1fd
Alessio Moretti <alessio.moretti@live.it>
2018-06-30 19:55:05 +0200

    Add support for dataset reading and events Kafka producer

--- client.py
+++ client.py
@@ -2,6 +2,42 @@
 This is the main of the client ingesting events into Kafka to power 
 the Stormbreak Flink datastream processing platform 
 """
+from constants import KAFKA_BOOTSTRAP_SERVER, KAFKA_MAX_QUEUE, SOURCES
+from datareader import combiningEventSources
 
 from confluent_kafka import Producer
 
+producer_config = {
+        'bootstrap.servers': KAFKA_BOOTSTRAP_SERVER,
+        'queue.buffering.max.messages': KAFKA_MAX_QUEUE, 
+        'queue.buffering.max.ms' : 100
+}
+
+def deliveryCallback(err, msg):
+        if err:
+            print("[ERROR] message failed delivery: {}".format(err))
+        else:
+            print("[PRODUCER LOG] message delivered {} - partition {} with offset {}".format(msg.topic(), msg.partition(), msg.offset()))
+        
+def producingEvent(timestamps, records):
+    # instantiating producer
+    producer = Producer(producer_config)
+
+    # producing sequential events
+    counter = 0
+    print("[PRODUCER] STARTING PRODUCER")
+    for ts in timestamps:
+        for record in records[ts]:
+            record_topic = record[0]
+            record_key   = "{}:{}".format(record[0],record[1])
+            record_val   = record[2]
+            producer.produce(record_topic, record_key, record_val, callback=deliveryCallback)
+            producer.poll(0)
+            counter += 1
+
+    producer.flush()
+    print("[PRODUCER] PRODUCED {} MSG".format(counter))
+
+if __name__ == "__main__":
+    data = combiningEventSources(SOURCES)
+    producingEvent(data[0], data[1])
--- constants.py
+++ constants.py
@@ -7,5 +7,15 @@ TOPIC_FRIENDSHIP = "friendships"
 TOPIC_POSTS      = "posts"
 TOPIC_COMMENTS   = "comments"
 
+# datasets
+DATASET_FRIENDSHIP = "./datasets/friendships.dat"
+DATASET_POSTS      = "./datasets/posts.dat"
+DATASET_COMMENTS   = "./datasets/comments.dat"
+SOURCES            = [(DATASET_FRIENDSHIP, TOPIC_FRIENDSHIP), (DATASET_POSTS, TOPIC_POSTS), (DATASET_COMMENTS, TOPIC_COMMENTS)]
+
 # Kafka server configuration (assuming default port configuration)
-KAFKA_BOOTSTRAP_SERVER = "localhost"
+KAFKA_BOOTSTRAP_SERVER = "localhost:9092"
+KAFKA_MAX_QUEUE        = 100000
+
+# datetime format
+DATETIME_FORMAT  = "%Y-%m-%dT%H:%M:%S.%f"
